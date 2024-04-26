6bca0dfabe1d30eb1ffc371f4efba3713ddfcd69
cryptitalk <cryptitalk@cryptitalk.com>
2024-04-25 19:30:36 -0700

    first change to make everything work

--- pages/homepageComponents/menu.js
+++ pages/homepageComponents/menu.js
@@ -28,6 +28,7 @@ export default function Menu()
                     >
                         Home
                     </Navbar.Link>
+                    {/*
                     <Navbar.Link
                         className="uppercase block py-2 pr-4 pl-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-700 md:p-0 md:hover:text-white text-gray-400 hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700"
                         href="#section2">
@@ -38,11 +39,13 @@ export default function Menu()
                         href="#section3">
                         Section3
                     </Navbar.Link>
+                    */}
                     <Navbar.Link
                         className="uppercase block py-2 pr-4 pl-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-700 md:p-0 md:hover:text-white text-gray-400 hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700"
                         href="#section4">
-                        Section4
+                        Presale
                     </Navbar.Link>
+                    {/*
                     <Navbar.Link
                         className="uppercase block py-2 pr-4 pl-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-700 md:p-0 md:hover:text-white text-gray-400 hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700"
                         href="#section5">
@@ -68,6 +71,7 @@ export default function Menu()
                         href="#section9">
                         Section9
                     </Navbar.Link>
+                    */}
                 </Navbar.Collapse>
             </Navbar>
             {/* Menu END */}
--- pages/homepageComponents/section4.js
+++ pages/homepageComponents/section4.js
@@ -10,15 +10,10 @@ export default function Section4()
                     <div className="text-left">
                         <div className="box-cont h-fit w-fit px-14 mb-10 py-8 shadow-md bg-gradient-to-r from-neutral-900 rounded-lg">
                             <h3 className="text-white font-bold">
-                                🚀 Vestibulum venenatis vel elit  <br />
-                                non congue.
+                                🚀 Get ButterflyX device to run Depin
                             </h3>
-                            <p className="text-white"><strong>1) </strong> Pellentesque dapibus</p>
-                            <p className="text-white"><strong>2) </strong> Luctus lacus et pharetra</p>
-                            <p className="text-white"><strong>3) </strong> Curabitur laoreet</p>
-                            <p className="text-white"><strong>4) </strong> Sagittis iaculis.</p>
-                            <p className="text-white"><strong>5) </strong> Donec feugiat nunc nec volutpat gravida.</p>
-                            <p className="text-white mb-5"><strong>6) </strong> Integer luctus orci sed sem</p>
+                            <p className="text-white"><strong>1) </strong> Buy with USDT or BNB</p>
+                            <p className="text-white"><strong>2) </strong> Claim the device by minting NFT</p>
                         </div>
                     </div>
                     <SeedSale />
