07055d70bc7c3f0d11db169fb960f27910e87ab0
cryptitalk <148308031+cryptitalk@users.noreply.github.com>
2024-06-02 17:30:58 -0700

    Update home.js

--- pages/homepageComponents/home.js
+++ pages/homepageComponents/home.js
@@ -18,7 +18,7 @@ export default function HomeSection() {
                             <iframe
                                 width="560"
                                 height="315"
-                                src="https://www.youtube.com/embed/pGyi4Ki71qc"
+                                src="https://www.youtube.com/embed/MbSJaAMSErU"
                                 frameBorder="0"
                                 allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                                 allowFullScreen
